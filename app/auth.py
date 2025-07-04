2dc5c450d19b1890485ef54cc3249920769fde29
alejandrojmnzz <alejandro12850mj@gmail.com>
2025-07-04 19:18:26 +0000

    Implementado el endpoint de inicio de sesion

--- app/auth.py
+++ app/auth.py
@@ -1,16 +1,28 @@
 from flask import Blueprint, request, jsonify
 import re
 from .database import db
-from .models import Usuario
+from .models import Usuario, verificar_contrasena
 
 auth_bp = Blueprint("auth", __name__)
 
 @auth_bp.route("/login", methods = ["POST", "GET"])
 def iniciar_sesion():
-    """
-    Aqui va el codigo de Ale, no pude hacer mas que eso porque
-    tu vas a validar los correos
-    """
+    body = request.json
+    correo = body.get("correo", None)
+    contrasena = body.get("contrasena", None)
 
+    usuario = User.query.filter_by(correo = coreo).first()
+    if usuario is None:
+        return jsonify("El usuario no existe"), 404
+    else:
+        try:
+            if usuario.verificar_contrasena(contrasena):
+                return jsonify({"usuario": usuario.serialize()}), 200
+            else:
+                return jsonify("Credenciales incorrectas"), 404
+        except Exception as error:
+            return jsonify("Error"), 500
+
+    pass
     if not correo or not contrasena:
         return jsonify({"ERROR": "Correo o contraseña invalidos"}), 400
