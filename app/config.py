3d04b64ec73fc3d40adbfff7d1f2f3e0e2e4d71e
Cristopher Garcia <garcacristopher3@gmail.com>
2025-07-17 20:16:23 -0500

    update

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Estan como ejemplo, cambiar en produccion
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://diego:123456@localhost:5432/trelloop_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@localhost:5432/trello-clone-grupo-1")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "desarrollo-secret-key-cambiar-en-produccion")
 CORS_ORIGINS = os.getenv("CORS_ORIGINS", "http://localhost:3000,http://localhost:5173,http://localhost:5174").split(",")
 
