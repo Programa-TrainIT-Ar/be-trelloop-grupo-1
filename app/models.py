054df60525d5e7fbb210722b09a80d3c8a98622c
Fernando Sepúlveda <fsepulveda.87@gmail.com>
2025-08-20 21:29:53 -0400

    models.py, se modifica modelo de datos de notificaciones

--- app/models.py
+++ app/models.py
@@ -157,7 +157,7 @@ class Card(db.Model):
 class Notification(db.Model):
     __tablename__ = "notifications"
 
-    id = db.Column(db.Integer, primary_key=True)  
+    id = db.Column(db.String(36), primary_key=True) 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     # Tipo y contenido
